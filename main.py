62228ed6accab942dddf0f7ca591e05b0880bd66
zyan-007 <zyanprogramming@gmail.com>
2026-04-04 17:07:21 +0530

    feat: Add file persistence using JSON

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-tasks = []
+import json
+
+FILE = "tasks.json"
+
+def load_tasks():
+    try:
+        with open(FILE, "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def save_tasks(tasks):
+    with open(FILE, "w") as f:
+        json.dump(tasks, f)
+
+tasks = load_tasks()
 
 def add_task(task):
     tasks.append({"task": task, "done": False})
+    save_tasks(tasks)
 
 def show_tasks():
     for i, t in enumerate(tasks):
@@ -11,6 +27,7 @@ def show_tasks():
 def complete_task(index):
     if 0 <= index < len(tasks):
         tasks[index]["done"] = True
+        save_tasks(tasks)
 
 def main():
     while True:
